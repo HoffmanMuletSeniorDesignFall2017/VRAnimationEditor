7376eadea50a67c8811e058326932745c78dcebb
Alec Hoffmann <astrohoff@ufl.edu>
2017-10-02 13:25:57 -0400

    Explored hidden EditorVR gameobjects. Tried unsuccessfully to get workspace ui to scale nicely.

--- VRAnimationEditor/Assets/Scripts/VR/AssetSelectionWorkspace/AssetSelectionWorkspace.cs
+++ VRAnimationEditor/Assets/Scripts/VR/AssetSelectionWorkspace/AssetSelectionWorkspace.cs
@@ -9,19 +9,73 @@ class AssetSelectionWorkspace : Workspace {
     [SerializeField]
     public GameObject uiPrefab;
 
+
     public override void Setup()
     {
+
         // Set inital workspace bounds.
         m_CustomStartingBounds = new Vector3(0.6f, MinBounds.y, 0.4f);
 
         base.Setup();
 
         // Instantiate ui and set its transform.
-        GameObject ui = this.InstantiateUI(uiPrefab);
-        ui.transform.SetParent(m_WorkspaceUI.topFaceContainer, false);
-        ui.transform.localPosition = new Vector3(0, 0, 0);
-        ui.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
+        GameObject ui = this.InstantiateUI(uiPrefab, m_WorkspaceUI.topFaceContainer, false);;
 
+        preventResize = true;
+
+
+        /*if(firstSetup){
+            Debug.Log("Spawning workspace copy");
+            Instantiate(gameObject);
+        }
+        else{
+            Debug.Log("Workspace already copied");
+        }*/
+        //Transform root = GetRoot();
+        //LongDebugLog(GetHierarchyString(root));
 
     }
+
+    private Transform GetRoot(){
+        Transform root = transform;
+        while(root.parent != null){
+            root = root.parent;
+        }
+        return root;
+    }
+
+    private string GetHierarchyString(Transform trsfm, int level = 0){
+        string str = "";
+        for(int i = 0; i < level; i++){
+            str += "  ";
+        }
+        str += trsfm.name;
+        if(trsfm == transform){
+            str += " *";
+        }
+        str += "\n";
+        for(int i = 0; i < trsfm.childCount; i++){
+            str += GetHierarchyString(trsfm.GetChild(i), level + 1);
+        }
+        return str;
+    }
+
+    private void LongDebugLog(string message){
+        string[] lines = message.Split('\n');
+        int chunkLineCount = 0;
+        string chunk = "";
+        for(int i = 0; i < lines.Length; i++){
+            chunk += lines[i] + '\n';
+            chunkLineCount++;
+            if(chunkLineCount > 450){
+                Debug.Log(chunk);
+                chunkLineCount = 0;
+                chunk = "";
+            }
+        }
+        if(chunkLineCount > 0){
+            Debug.Log(chunk);
+        }
+    }
+        
 }
---
+++ VRAnimationEditor/Assets/Scripts/VR/AssetSelectionWorkspace/WorkspaceUiScaleMaintainer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.EditorVR;
+
+public class WorkspaceUiScaleMaintainer : MonoBehaviour  {
+    private Vector3 initScale;
+    private Rect initRect;
+    private RectTransform rectTrsfm;
+    private bool isInitialized = false;
+
+    public void Init () {
+        initScale = transform.lossyScale;
+        if(initScale == Vector3.zero){
+            return;
+        }
+        rectTrsfm = GetComponent<RectTransform>();
+        initRect = rectTrsfm.rect;
+        isInitialized = true;
+	}
+	
+    public void OnBoundsChange () {
+        if(!isInitialized){
+            Init();
+            return;
+        }
+        Vector3 currentScale = transform.lossyScale;
+
+        if(currentScale != initScale){
+            float horizontalChange = currentScale.x / initScale.x;
+            float verticalChange = currentScale.y / initScale.y;
+
+            Vector3 newLocalScale = transform.localScale;
+            newLocalScale.x /= horizontalChange;
+            newLocalScale.y /= verticalChange;
+            //transform.localScale = newLocalScale;
+
+            rectTrsfm.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, initRect.size.x / horizontalChange);
+            rectTrsfm.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, initRect.size.y / verticalChange);
+        }
+	}
+}
