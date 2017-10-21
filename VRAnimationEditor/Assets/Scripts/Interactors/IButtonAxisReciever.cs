78749cfb9bd4ccbdef6cb6979058c5ecb2dac52c
Alec Hoffmann <astrohoff@ufl.edu>
2017-10-21 14:11:49 -0400

    Modified input architecture so that there are separate pointing, grabbing, and button recieving interfaces. Added quick-fixes to existing code to get everything working with this.

---
+++ VRAnimationEditor/Assets/Scripts/Interactors/IButtonAxisReciever.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+public interface IButtonAxisReciever
+{
+	void OnRecieveButton (int sourceID, int buttonID, bool buttonState);
+	void OnRecieveAxis(int sourceID, int axisID, float axisValue);
+}
