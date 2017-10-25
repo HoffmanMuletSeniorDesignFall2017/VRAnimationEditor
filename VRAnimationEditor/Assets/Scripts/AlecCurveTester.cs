02a2c2be51053ecf9a7f58706bbc3792353695ba
Alec Hoffmann <astrohoff@ufl.edu>
2017-10-25 14:30:11 -0400

    More reoganization. Bindings test.

---
+++ VRAnimationEditor/Assets/Scripts/AlecCurveTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AlecCurveTester : MonoBehaviour {
+    public AnimationClip clip;
+
+	// Use this for initialization
+	void Start () {
+        PrintBindingInfo();
+	}
+	
+    private void PrintBindingInfo(){
+        string debug = "";
+        EditorCurveBinding[] bindings = AnimationUtility.GetCurveBindings(clip);
+        for (int i = 0; i < bindings.Length; i++)
+        {
+            debug += "binding " + i + ": path = " + bindings[i].path + ", property name = " + bindings[i].propertyName + "\r\n";
+        }
+        Debug.Log(debug);
+    }
+}
