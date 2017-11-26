474fc62d319828676946e71d11675e4fc182bc1d
muletmiles <muletmiles@ufl.edu>
2017-11-26 15:14:01 -0500

    Added a button to toggle nodes and transparent material

---
+++ VRAnimationEditor/Assets/TogglePreview.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePreview : MonoBehaviour {
+
+    private NodeVisualizationManager nvm;
+    public GameObject modelSpawnAnchor;
+
+    bool toggled = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+
+        if (nvm == null)
+            nvm = modelSpawnAnchor.GetComponentInChildren<NodeVisualizationManager>();
+
+        if (!toggled)
+        {
+            nvm.RestoreMaterials();
+            nvm.HideNodeMarkers();
+            toggled = true;
+        }
+        else
+        {
+            nvm.ReplaceMaterialsExternal();
+            nvm.ShowNodeMarkers();
+            toggled = false;
+        }
+    }
+}
