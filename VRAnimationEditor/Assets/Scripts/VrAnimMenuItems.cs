61db360afca4de1bc98a71f0bcfdbe2ea1899166
Alec Hoffmann <astrohoff@ufl.edu>
2017-11-27 13:08:59 -0500

    Added menu item to launch VR editor from any scene (does not currently allow you to play other scenes after running editor scene).

---
+++ VRAnimationEditor/Assets/Scripts/VrAnimMenuItems.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+
+public static class VrAnimMenuItems{
+    public static string sceneName = "miles Test Scene 4";
+    public static string returnSceneName;
+
+    [MenuItem("Tools/VR Animation Editor")]
+	public static void RunVRAnimationEditor()
+    {
+        returnSceneName = EditorSceneManager.GetActiveScene().name;
+        string[] guids = AssetDatabase.FindAssets("\"" + sceneName + "\"");
+        string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        SceneAsset editorScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(path);
+        EditorSceneManager.playModeStartScene = editorScene;
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+}
